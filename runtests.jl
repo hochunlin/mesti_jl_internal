e70a2f3e79c0f136e3847d8e80cb15cc2692820a
Ho-Chun Lin <44913081+hochunlin@users.noreply.github.com>
2023-10-12 13:56:16 -0700

    Delete runtests.jl

--- runtests.jl
+++
@@ -1,8 +0,0 @@
-# Include MESTI module
-include("MESTI.jl")
-using .MESTI
-using LinearAlgebra, SparseArrays, Test
-
-include("matrix_solver_test.jl")
-include("interface_t_r_test.jl")
-include("unitary_test.jl")
